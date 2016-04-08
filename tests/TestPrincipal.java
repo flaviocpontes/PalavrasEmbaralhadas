bedcc952fa37488053b84ee5cdb825d41cc4f55b
Flávio Pontes <flaviopontes@acerp.org.br>
2016-04-08 14:13:56 -0300

    Primeiro commit, com a estrutura básica do projeto.

---
+++ tests/TestPrincipal.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+public class TestPrincipal {
+
+    @Test
+    public void testNoArgs(){
+        fail("Falhou porque eu quis!");
+    }
+}
