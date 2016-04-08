b55cb3045d0cdf831b44960373b6e5e62301cdaf
Flávio Pontes <flaviopontes@acerp.org.br>
2016-04-08 19:17:40 -0300

    Adição do PDF com o enunciado; Início da definição do método main();

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 
 public class Principal {
 
     public static void main (String[] args) {
+        System.out.println("Bem vindo ao jogo Palavras Embaralhadas!");
+        System.out.println("Bem vindo ao jogo Palavras Embaralhadas!");
+        Scanner in = new Scanner(System.in);
 
     }
 
